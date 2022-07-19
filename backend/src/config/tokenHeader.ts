89f21308bf9994d35deb29e2384cbf1f23020998
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-19 15:25:28 -0300

    feat: Add refreshUser feature, with route, useCase, test and Controller

--- backend/src/config/tokenHeader.ts
+++ backend/src/config/tokenHeader.ts
@@ -1,6 +1,6 @@
 export default {
     refreshToken: 'x-refresh-token',
-    // accessToken: 'x-access-token',
+    accessToken: 'x-access-token',
     // recoverPasswordToken: 'x-recover-password-token',
     // confirmEmailToken: 'x-confirm-email-token',
     // updateEmailToken: 'x-update-email-token',
---
+++ backend/src/modules/user/useCases/refreshUser/refreshUserUseCase.spec.ts
@@ -0,0 +1,94 @@
+import 'reflect-metadata'
+import { sign } from "jsonwebtoken"
+
+import { AppError } from "../../../../errors/AppError"
+import { ErrorMessages } from '../../../../errors/ErrorMessages'
+import { InMemoryTokenRepository } from '../../../token/repositories/inMemory/InMemoryTokensRepository'
+import { InMemoryUsersRepository } from '../../repositories/inMemory/InMemoryUsersRepository'
+import { RefreshUserUseCase } from './refreshUserUseCase'
+import envConfig from '../../../../config/envConfig'
+import dayjs from 'dayjs'
+
+let refreshTokenRepositoryInMemory: InMemoryTokenRepository
+let usersRepositoryInMemory: InMemoryUsersRepository
+let refreshUserUseCase: RefreshUserUseCase
+
+describe('Create refresh Token', () => {
+
+    beforeEach(() => {
+        refreshTokenRepositoryInMemory = new InMemoryTokenRepository()
+        usersRepositoryInMemory = new InMemoryUsersRepository()
+        refreshUserUseCase = new RefreshUserUseCase(usersRepositoryInMemory, refreshTokenRepositoryInMemory)
+    })
+
+    it('should be able to create a new refresh token if a refresh token already exists', async () => {
+
+        const providerId = '3rd-part-userId'
+        const name = 'userNameTest'
+
+        await usersRepositoryInMemory.create({ name, providerId })
+
+        const user = await usersRepositoryInMemory.findByProviderId(providerId)
+
+        const accessToken = sign({}, envConfig.JWT_SECRET, {
+            subject: user.id,
+            expiresIn: envConfig.JWT_expiration
+        })
+
+        const refreshToken = await refreshTokenRepositoryInMemory.create({
+            value: 'valid_refresh_token',
+            expiresAt: dayjs().add(60 * 15, 'seconds').toDate(),  // 15 minutes
+            userId: user.id,
+        })
+
+        const refreshUser = await refreshUserUseCase.execute({
+            accessToken,
+            refreshTokenValue: refreshToken.value
+        })
+
+        expect(refreshUser).toHaveProperty('accessToken')
+        expect(refreshUser).toHaveProperty('refreshToken')
+        expect(refreshUser).toHaveProperty('user')
+
+    })
+
+    it('should not be able to create a refresh token for an invalid access token', async () => {
+
+        const invalidAccessToken = sign({}, envConfig.JWT_SECRET, {
+            subject: 'invalid-userId',
+            expiresIn: envConfig.JWT_expiration
+        })
+
+        await expect(
+            refreshUserUseCase.execute({
+                refreshTokenValue: 'valid_refresh_token',
+                accessToken: invalidAccessToken
+            })).rejects.toEqual(new AppError(ErrorMessages.userNotFound, 404))
+    })
+
+    it('should not be able to create a new refresh token if old one is expired', async () => {
+        const providerId = '3rd-part-userId'
+        const name = 'userNameTest'
+
+        await usersRepositoryInMemory.create({ name, providerId })
+
+        const user = await usersRepositoryInMemory.findByProviderId(providerId)
+
+        const accessToken = sign({}, envConfig.JWT_SECRET, {
+            subject: user.id,
+            expiresIn: envConfig.JWT_expiration
+        })
+
+        await refreshTokenRepositoryInMemory.create({
+            value: 'valid_refresh_token',
+            expiresAt: dayjs().add(-60 * 15, 'seconds').toDate(),  // 15 minutes
+            userId: user.id,
+        })
+
+        await expect(
+            refreshUserUseCase.execute({
+                refreshTokenValue: 'valid_refresh_token',
+                accessToken: accessToken
+            })).rejects.toEqual(new AppError(ErrorMessages.tokenNotFound, 404))
+    })
+})
