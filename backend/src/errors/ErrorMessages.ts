f4c25c92010fee48a7ff825df788de5a02dd1de8
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-20 16:46:08 -0300

    feat: Throw an error if trying to remove a pokemon that is not in the team

--- backend/src/errors/ErrorMessages.ts
+++ backend/src/errors/ErrorMessages.ts
@@ -4,6 +4,8 @@ export const ErrorMessages = {
     tokenNotFound: 'Token not Found!',
     pokemonAlreadyOnTeam: 'Pokemon already in this team',
     teamComplete: 'Cannot Add more pokemons to this team',
+    pokemonNotFound: 'Pokemon not Found!',
+    pokemonNotOnTeam: 'Pokemon in not on this team',
     // invalidToken: 'Invalid Token!',
     // missingToken: 'Token Missing!',
     // incorrectLoginOrPassword: 'Login or Password Invalid!',
