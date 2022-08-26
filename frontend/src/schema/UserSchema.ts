8376b37b8c705544686f3181e264a8300b93059d
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-08-26 16:53:44 -0300

    update: update pokemon and user schemas

--- frontend/src/schema/UserSchema.ts
+++ frontend/src/schema/UserSchema.ts
@@ -1,10 +1,22 @@
 import { PokemonSchema, TeamSchema } from "./PokemonSchema"
 
+export interface UserResponseProps {
+    id: string
+    name: string
+    providerId: string
+    avatarURL: string
+    team: {
+        id: string
+        userId: string
+        pokemons: {
+            pokemon: PokemonSchema
+        }[]
+    }
+}
 export interface UserSchema {
     id: string
     name: string
     providerId: string
     avatarURL: string
     team: TeamSchema
-
 }
