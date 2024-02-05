4d78f2cab91963d998ccdbf98cfcab0e893fd5b9
Fernando Cardozo <fhpcardozo@gmail.com>
2024-02-05 14:44:59 -0300

    update: nextLink

--- frontend/src/components/Profile/index.tsx
+++ frontend/src/components/Profile/index.tsx
@@ -4,29 +4,35 @@ import { useUser } from "../../hooks/useUser";
 import { ProfileContainer } from "./style";
 
 export function Profile(): JSX.Element {
+  const { user } = useUser();
 
-    const { user } = useUser()
+  // TODO: Check if referrerPolicy is needed when deployeds
 
-    // TODO: Check if referrerPolicy is needed when deployeds
-
-    return (
-        <ProfileContainer>
-            <div className="imgContainer">
-                {user
-                    ? user.avatarURL
-                        ? <img referrerPolicy="no-referrer" src={user.avatarURL} alt="profile" />
-                        : <img src="/avatar/user.png" alt="profile" />
-                    : <img src="/avatar/user.png" alt="profile" />
-                }
-            </div>
-            {user && (
-                <>
-                    <h4 className="profileName">{user.name}</h4>
-                    <Link href="/user-settings">
-                        <a className="profileUpdateButton">Update Profile</a>
-                    </Link>
-                </>
-            )}
-        </ProfileContainer>
-    )
+  return (
+    <ProfileContainer>
+      <div className="imgContainer">
+        {user ? (
+          user.avatarURL ? (
+            <img
+              referrerPolicy="no-referrer"
+              src={user.avatarURL}
+              alt="profile"
+            />
+          ) : (
+            <img src="/avatar/user.png" alt="profile" />
+          )
+        ) : (
+          <img src="/avatar/user.png" alt="profile" />
+        )}
+      </div>
+      {user && (
+        <>
+          <h4 className="profileName">{user.name}</h4>
+          <Link href="/user-settings" className="profileUpdateButton">
+            Update Profile
+          </Link>
+        </>
+      )}
+    </ProfileContainer>
+  );
 }
