198913ee71f66dede95a9d050e9907ba1fca3f2b
Fernando Cardozo <fhpcardozo@gmail.com>
2024-02-05 13:48:51 -0300

    update: aws-sdk and nextjs versions

--- backend/src/shared/providers/storageProvider/implementations/S3StorageProvider.ts
+++ backend/src/shared/providers/storageProvider/implementations/S3StorageProvider.ts
@@ -1,47 +1,49 @@
-import { S3 } from "aws-sdk";
+import * as AWS from "@aws-sdk/client-s3";
 import fs from "fs";
 import { IStorageProvider } from "../IStorageProvider";
-import mime from 'mime'
+import mime from "mime";
 import { resolve } from "path";
 import { tmpUploadFolder } from "../../../../config/tmpUploadFolder";
 
 export class S3StorageProvider implements IStorageProvider {
+  private client: AWS.S3;
 
-    private client: S3
+  constructor() {
+    this.client = new AWS.S3({
+      region: process.env.AWS_BUCKET_REGION,
+    });
+  }
 
-    constructor() {
-        this.client = new S3({
-            region: process.env.AWS_BUCKET_REGION
-        })
-    }
+  async save(folder: string, file: string): Promise<string> {
+    const originalName = resolve(tmpUploadFolder, file);
 
-    async save(folder: string, file: string): Promise<string> {
+    const fileContent = await fs.promises.readFile(originalName);
 
-        const originalName = resolve(tmpUploadFolder, file)
+    const ContentType = mime.getType(originalName);
 
-        const fileContent = await fs.promises.readFile(originalName)
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: "public-read",
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
 
-        const ContentType = mime.getType(originalName)
+    await fs.promises.unlink(originalName);
 
-        await this.client.putObject({
-            Bucket: `${process.env.AWS_BUCKET}/${folder}`,
-            Key: file,
-            ACL: 'public-read',
-            Body: fileContent,
-            ContentType,
-        }).promise()
+    const imageURL = `${process.env.AWS_BUCKET_URL}/${folder}/${file}`;
 
-        await fs.promises.unlink(originalName)
+    return imageURL;
+  }
 
-        const imageURL = `${process.env.AWS_BUCKET_URL}/${folder}/${file}`
-
-        return imageURL
-    }
-
-    async delete(folder: string, file: string): Promise<void> {
-        await this.client.deleteObject({
-            Bucket: `${process.env.AWS_BUCKET}/${folder}`,
-            Key: file,
-        }).promise()
-    }
+  async delete(folder: string, file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
+  }
 }
